ee89d13eff0c7132e801ae5ed3a6c143eea0d93f
tanvipnabar <tanvipnabar@gmail.com>
2013-04-23 17:19:45 -0400

    commit

---
+++ Tests/Backbone/example.js
@@ -0,0 +1,23 @@
+var Todo = Backbone.Model.extend({
+  // Default todo attribute values
+  defaults: {
+	title: '',
+    completed: false
+  },
+  initialize: function(){
+    console.log('This model has been initialized.');
+    this.on('change', function(){
+        console.log('- Values for this model have changed.');
+    });
+} });
+
+var myTodo = new Todo();
+myTodo.set('title', 'The listener is triggered whenever an attribute value changes.');
+console.log('Title has changed: ' + myTodo.get('title'));
+myTodo.set('completed', true);
+console.log('Completed has changed: ' + myTodo.get('completed'));
+
+myTodo.set({
+  title: 'Changing more than one attribute at the same time only triggers thelistener once.', 
+  completed: true
+});
